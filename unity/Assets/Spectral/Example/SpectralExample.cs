ab4b41a0385d000d470b99eee9548549c072504b
Jonathan Potts <jonathanpotts@outlook.com>
2018-06-19 15:39:57 -0700

    Added example implementation for Unity plugin.

---
+++ unity/Assets/Spectral/Example/SpectralExample.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using Spectral;
+
+public class SpectralExample : MonoBehaviour
+{
+    void Start()
+    {
+        if (Led.Initialize())
+        {
+            Led.SetColor(Color.black);
+            Led.SetColorForLeds(
+                new[] { LedName.W, LedName.A, LedName.S, LedName.D }, 
+                Color.magenta
+            );
+        }
+    }
+
+    void OnDestroy()
+    {
+        Led.Shutdown();
+    }
+}
